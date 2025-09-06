8e0e1260f2072cffe859b175416e8a873d0b8570
jackycamp <jackcampanella@protonmail.com>
2025-09-06 17:35:25 -0400

    wip xml parser

--- narfle/XMLParser.swift
+++ narfle/XMLParser.swift
@@ -1,41 +1,85 @@
 import Foundation
 
 enum XMLParserError: Error {
-    case invalidEncoding
+    case invalidEncoding(String?)
+    case elementMismatch(String?)
 }
 
-class XMLParser: NSObject, XMLParserDelegate {
+struct XMLElement {
+    let name: String
+    let attributes: [String: String]
+    var text: String?
+    let namespace: String?
+}
+
+class XMLParser: NSObject {
+    private var elements: [XMLElement] = []
+    private var currentElement: XMLElement?
+    private var error: XMLParserError?
+
     func parse(_ xmlString: String) throws {
         guard let data = xmlString.data(using: .utf8) else {
-            throw XMLParserError.invalidEncoding 
+            throw XMLParserError.invalidEncoding("Only UTF-8 encoding is supported")
         }
 
         let parser = Foundation.XMLParser(data: data)
         parser.delegate = self
 
+        // FIXME: check if error was set
         parser.parse()
+
+        print("is there an error? \(error)")
     }
 
-    func findElement(_ elementName: String) {
+    func findFirst(name: String) -> XMLElement? {
+        return elements.first(where: { $0.name == name })
 
     }
 
-    // XMLParserDelegate callbacks called by Foundation's XMLParser
-    // as it encounters certain content - as if triggering eventsj
+    func find(name: String) -> [XMLElement] { 
+        return elements.filter { $0.name == name}
+    }
 
+    func numElements() -> Int {
+        return elements.count
+    }
+}
+
+// Extends the base XMLParser. Implements the XMLParserDelegate parser functions
+// required by Foundation.XMLParser
+//
+// FIXME: this has no concept of the inherent tree structure
+// that is there is no concept of children
+extension XMLParser: XMLParserDelegate {
     // Called when parser encounters an opening XML tag
     // Captures element name including element prefix like `dc:title`
     func parser(_ parser: Foundation.XMLParser, didStartElement elementName: String, namespaceURI: String?, 
                 qualifiedName qName: String?, attributes attributeDict: [String : String] = [:]) {
+
         print("elementName: \(elementName)")
         print("namespaceURI: \(namespaceURI)")
         print("qName: \(qName)")
         print("attributes \(attributeDict)")
+
+        var newElement = XMLElement(
+            name: qName ?? elementName,
+            attributes: attributeDict,
+            namespace: namespaceURI,
+        )
+
+        currentElement = newElement
       }
 
     // Called when parser encounters text content between XML tags
     func parser(_ parser: Foundation.XMLParser, foundCharacters string: String) {
         print("foundCharacters \(string)")
+        guard var current = currentElement else {
+            error = XMLParserError.elementMismatch("Found characters for element but currentElement is not set")
+            parser.abortParsing()
+            return
+        }
+        current.text = string
+        currentElement = current
     }
 
     // Called when parser encounters closing XML tag
@@ -44,5 +88,23 @@ class XMLParser: NSObject, XMLParserDelegate {
         print("namespaceURI \(namespaceURI)")
         print("qName \(qName)")
 
+        let name = qName ?? elementName
+
+        guard var current = currentElement else {
+            error = XMLParserError.elementMismatch("Got end tag for element: \(name) but we aren't processing a currentElement")
+            parser.abortParsing()
+            return
+        }
+
+        if name != current.name {
+            error = XMLParserError.elementMismatch("Got end tag for element: \(name) which does not match currentElement: \(current.name)")
+            parser.abortParsing()
+            return
+        }
+        print("capturing element \(current)")
+
+        elements.append(current)
+        currentElement = nil
     }
+
 }
