1073d31566692d903e311ab6edbac506a8236b10
jackycamp <jackcampanella@protonmail.com>
2025-09-17 17:12:43 +0200

    registering models with modelContainer

--- narfle/narfleApp.swift
+++ narfle/narfleApp.swift
@@ -6,6 +6,7 @@
 //
 
 import SwiftUI
+import SwiftData
 
 @main
 struct narfleApp: App {
@@ -13,5 +14,6 @@ struct narfleApp: App {
         WindowGroup {
             ContentView()
         }
+        .modelContainer(for: [NarfBundle.self])
     }
 }
