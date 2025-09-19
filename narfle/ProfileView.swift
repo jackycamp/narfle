e94dfc44f29f2d687c343e9696208fbf85d39617
jackycamp <jackcampanella@protonmail.com>
2025-09-19 17:06:15 +0200

    scaffolding for ProfileView

---
+++ narfle/ProfileView.swift
@@ -0,0 +1,13 @@
+import SwiftUI
+
+struct ProfileView: View {
+    @State private var showDevInfo = false
+
+    var body: some View {
+        VStack {
+            Text("Profile")
+            .font(.title3)
+        }
+    }
+}
+
