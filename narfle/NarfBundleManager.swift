2c8db015c4dac8929a5b051dd3a4299b7d748a99
jackycamp <jackcampanella@protonmail.com>
2025-09-23 06:38:15 -0400

    WIP NarfBundleManager

---
+++ narfle/NarfBundleManager.swift
@@ -0,0 +1,9 @@
+struct NarfBundleManager {
+    static func isFileTypeSupported(fileURL: URL) -> bool {
+        //
+    }
+
+    static func toBundle(fileURL: URL) throws -> NarfBundle {
+        //
+    }
+}
